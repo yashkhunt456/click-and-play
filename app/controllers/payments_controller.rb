2c2d92bbb2fee1c69de3b35ef6d63cadad858fa0
Yash Kunt <trainee2.logicalstreet@gmail.com>
2025-02-17 06:03:04 +0530

    add payment

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,50 +1,51 @@
 class PaymentsController < ApplicationController
-    before_action :authenticate_user!
-    before_action :set_booking
-    before_action :set_boxhouse_and_box
-  
-    def new
-      # Build a new payment object for the booking but don't save it yet
-      @payment = @booking.payment || @booking.build_payment(amount: @booking.total_price, status: "pending")
-    end
-  
-    def create
-      @payment = @booking.payment # Fetch the existing payment for the booking
+  before_action :authenticate_user!
+  before_action :set_booking
+  before_action :set_boxhouse_and_box
 
-      begin
-        # Use the amount directly (no need to multiply by 100)
-        charge = Stripe::Charge.create(
-          amount: @payment.amount,  # amount is already in integer form
-          currency: 'usd',
-          source: params[:stripeToken],
-          description: "Payment for Booking ##{@booking.id}"
-        )
-  
-        # Update payment with the charge ID and status
-        @payment.update(
-          stripe_payment_id: charge.id,
-          status: "succeeded"
-        )
-  
-        # Redirect to the booking page with success message
-        redirect_to booking_path(@booking), notice: "Payment successful!"
-      rescue Stripe::CardError => e
-        # If payment fails, update payment status and show error
-        @payment.update(status: "failed")
-        flash[:alert] = e.message
-        redirect_to new_booking_payment_path(@booking)
-      end
-    end
-  
-    private
-  
-    def set_booking
-      @booking = Booking.find(params[:booking_id])
-    end
+  def new
+    # Build a new payment object for the booking but don't save it yet
+    @payment = @booking.build_payment(amount: @booking.total_price, status: "pending")
+  end
 
-    def set_boxhouse_and_box
-        @boxhouse = Boxhouse.find(params[:boxhouse_id])
-        @box = @boxhouse.boxes.find(params[:box_id])
-     end
+  def create
+    @payment = @booking.payment || @booking.build_payment(amount: @booking.total_price, status: "pending") # Create new payment if it doesn't exist
+    
+    begin
+      # Use the amount directly (no need to multiply by 100)
+      charge = Stripe::Charge.create(
+        amount: @payment.amount,  # amount is already in integer form
+        currency: 'usd',
+        source: params[:stripeToken],
+        description: "Payment for Booking ##{@booking.id}"
+      )
+
+      # Update payment with the charge ID and status
+      @payment.update(
+        stripe_payment_id: charge.id,
+        status: "succeeded"
+      )
+
+      @booking.confirm_booking
+
+      # Redirect to the booking page with success message
+      redirect_to boxhouse_box_booking_path(@boxhouse, @box, @booking), notice: "Payment successful!"
+    rescue Stripe::CardError => e
+      # If payment fails, update payment status and show error
+      @payment.update(status: "failed")
+      flash[:alert] = e.message
+      redirect_to new_booking_payment_path(@booking)
+    end
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:booking_id])
+  end
+
+  def set_boxhouse_and_box
+    @boxhouse = Boxhouse.find(params[:boxhouse_id])
+    @box = @boxhouse.boxes.find(params[:box_id])
+  end
 end
-  
--- app/models/boxhouse.rb
+++ app/models/boxhouse.rb
@@ -1,20 +1,33 @@
-class Boxhouse < ApplicationRecord
-  belongs_to :user
-  has_many :boxes, dependent: :destroy
-  # has_many :slots, through: :boxes
-  # has_many :bookings, through: :slots
-  # has_many :payments, through: :bookings
+  class Boxhouse < ApplicationRecord
+    belongs_to :user
+    has_many :boxes, dependent: :destroy
+    has_one_attached :image do |attachable|
+    attachable.variant :thumb, resize_to_fill: [500, 300]
+  end
 
-  validates :name, :phone, :description, :timing, presence: true
-  validates :phone, numericality: { only_integer: true },
-   length: { is: 10 }, uniqueness: { message: "this phone number is already exist!"}
+    validates :name, :phone, :description, :timing, presence: true
+    validates :phone, numericality: { only_integer: true },
+    length: { is: 10 }, uniqueness: { message: "this phone number is already exist!"}
 
-   after_create :assign_boxhouse_role
+    after_create :assign_boxhouse_role
 
-   private
+    def self.ransackable_attributes(auth_object = nil)
+      ["address", "created_at", "name", "updated_at", "user_id"]
+    end
 
-   def assign_boxhouse_role
-    self.user.add_role(:boxhouse)
-   end
+    def self.ransackable_associations(auth_object = nil)
+      ["boxes", "image_attachment", "image_blob", "user"]
+    end
 
-end
+    def large_image
+      image.variant(resize_to_fill: [800, 600]).processed
+    end
+    
+
+    private
+
+    def assign_boxhouse_role
+      self.user.add_role(:boxhouse)
+    end
+
+  end
